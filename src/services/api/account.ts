a08d221fad313571d0310cb301c56dba94bd65d7
Wenderson Pires <wendersonpdas@gmail.com>
2024-05-15 12:59:40 -0300

    added schema for get account service

--- src/services/api/account.ts
+++ src/services/api/account.ts
@@ -1,7 +1,22 @@
 import { URI } from "./API";
 
+type Accounts = {
+  id: string;
+  donors_count: number;
+  total_donations_in_usd: string;
+  total_donations_out_usd: string;
+  total_matching_pool_allocations_usd: string;
+};
+
+type GetAccountsResponse = {
+  count: number;
+  next?: string;
+  previous?: string;
+  results: Accounts[];
+};
+
 export const getAccounts = async () => {
   const res = await fetch(`${URI}/accounts`);
   const json = await res.json();
-  return json;
+  return json as GetAccountsResponse;
 };
