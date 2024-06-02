439b0fe89561ffbd59937ef4fe8f730a61ab9f51
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-06-02 23:44:15 +0400

    test: Update homepage tests

--- src/app/tests.tsx
+++ src/app/tests.tsx
@@ -1,17 +1,40 @@
-import { screen } from "@testing-library/react";
-import { expect, test } from "vitest";
+import { screen, waitFor } from "@testing-library/react";
+import { expect, test, vi } from "vitest";
 
 import { renderWithStore } from "./_store/testEnv";
 import Homepage from "./page";
 
-test("Homepage", () => {
-  renderWithStore(<Homepage />);
+// TODO: create separate testing env config and make its variables available globally for all tests
+vi.stubEnv("NEXT_PUBLIC_NETWORK", "mainnet");
+vi.stubEnv("NEXT_PUBLIC_SOCIAL_DB_CONTRACT_ID", "social.near");
 
-  expect(screen.getByText("Donate Randomly")).toBeDefined();
+renderWithStore(<Homepage />);
 
-  const firstFoundProjectTitle = screen
-    .getAllByTestId("project-card-title")
-    .at(0);
+test("Homepage", async () => {
+  expect(
+    screen.getByText("Donate Randomly"),
+    "random donation button",
+  ).toBeDefined();
 
-  expect(firstFoundProjectTitle?.textContent).toBeTruthy();
+  await waitFor(() =>
+    expect(
+      screen.getAllByTestId("project-card").at(0),
+      "project cards loaded",
+    ).toBeDefined(),
+  );
+
+  const projectTitles = screen.getAllByTestId("project-card-title");
+
+  expect(projectTitles.at(0)?.textContent, "project titles").toBeTruthy();
+
+  const projectFundraisingAmounts = screen.getAllByTestId(
+    "project-card-fundraising-amount",
+  );
+
+  await waitFor(() =>
+    expect(
+      projectFundraisingAmounts.at(0)?.textContent,
+      "project fundraising amounts",
+    ).toBeDefined(),
+  );
 });
