3c8a4d16e151556f5e6c122551dc858dd927f1ba
Wenderson Pires <wendersonpdas@gmail.com>
2024-06-07 15:07:24 -0300

    wip

--- src/common/lib/index.ts
+++ src/common/lib/index.ts
@@ -1,6 +1,5 @@
 export * from "./images";
 export * from "./_address";
 export * from "./formatWithCommas";
-export * from "./fetchNearPrice";
 export * from "./yoctosToUsdWithFallback";
 export * from "./yoctosToNear";
