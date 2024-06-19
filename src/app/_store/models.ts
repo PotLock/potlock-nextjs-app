73de04c70ce22714d9281145e2ff0211c4447fdd
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-06-20 03:56:02 +0400

    Remove irrelevant changes

--- src/app/_store/models.ts
+++ src/app/_store/models.ts
@@ -1,19 +1,16 @@
 import { Models } from "@rematch/core";
 
 import { auth } from "@/modules/auth/state";
-import { donationModel } from "@/modules/donation";
 import { navModel, profilesModel } from "@/modules/profile/models";
 
 export interface RootModel extends Models<RootModel> {
   auth: typeof auth;
-  donation: typeof donationModel;
   profiles: typeof profilesModel;
   nav: typeof navModel;
 }
 
 export const models: RootModel = {
   auth,
-  donation: donationModel,
   profiles: profilesModel,
   nav: navModel,
 };
