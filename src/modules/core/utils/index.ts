8b7e2727078942aa6bc918810420d0881f40c5e8
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-06-21 07:19:33 +0400

    Rollback yoctosToUsdWithFallback

--- src/modules/core/utils/index.ts
+++ src/modules/core/utils/index.ts
@@ -1,24 +1,5 @@
-import Big from "big.js";
-
 import { NearBalanceResponse } from "@/common/api/pagoda";
 import { bigNumToFloat } from "@/common/lib";
-import formatWithCommas from "@/common/lib/formatWithCommas";
-import { fetchNearPrice } from "@/common/services";
-
-export const yoctosToUsdWithFallback = async (
-  amountYoctos: string,
-  abbreviate?: boolean,
-): Promise<string> => {
-  const nearToUsd = await fetchNearPrice();
-
-  if (nearToUsd === undefined) return "Error loading price";
-
-  return nearToUsd
-    ? "~$" +
-        formatWithCommas(Big(amountYoctos).mul(nearToUsd).div(1e24).toFixed(2))
-    : formatWithCommas(Big(amountYoctos).div(1e24).toFixed(2)) +
-        (abbreviate ? "N" : " NEAR");
-};
 
 export const balanceToFloat = (
   amount: NearBalanceResponse["balance"]["amount"],
