1ddf0914df76c5fbedb8e1db350227d342a84088
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-07-02 03:16:54 +0400

    Fix attribute syntax

--- src/common/assets/svgs/twitter.tsx
+++ src/common/assets/svgs/twitter.tsx
@@ -10,8 +10,8 @@ const TwitterSvg = (props: any) => (
     {...props}
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M16.25 15.4041L10.9706 7.70919L10.9797 7.7164L15.7398 2.20117H14.1491L10.2713 6.69017L7.19196 2.20117H3.02012L7.9489 9.38538L7.94831 9.38477L2.75 15.4041H4.34071L8.65183 10.4098L12.0782 15.4041H16.25ZM6.56169 3.40143L13.969 14.2038H12.7084L5.29515 3.40143H6.56169Z"
       fill="#DBDBDB"
     />
