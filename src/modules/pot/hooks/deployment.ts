e133b1e415bd32ffbf72ae1d79f050f1635f70df
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-07-18 23:33:15 +0400

    wip

--- src/modules/pot/hooks/deployment.ts
+++ src/modules/pot/hooks/deployment.ts
@@ -1,7 +1,27 @@
+import { useMemo } from "react";
+
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
+import { walletApi } from "@/common/contracts";
+
+import { PotDeploymentInputs, potDeploymentSchema } from "../models";
+
 export const usePotDeploymentForm = () => {
-  const form = useForm();
+  const defaultValues = useMemo<Partial<PotDeploymentInputs>>(
+    () => ({
+      owner: walletApi.accountId,
+    }),
+
+    [],
+  );
+
+  const form = useForm<PotDeploymentInputs>({
+    resolver: zodResolver(potDeploymentSchema),
+    mode: "onChange",
+    defaultValues,
+    resetOptions: { keepDirtyValues: true },
+  });
 
   return { form };
 };
--- src/modules/pot/models/schemas.ts
+++ src/modules/pot/models/schemas.ts
@@ -1,3 +1,46 @@
-import { object } from "zod";
+import { infer as FromSchema, array, number, object, string } from "zod";
 
-export const potDeploymentSchema = object({});
+export const potDeploymentSchema = object({
+  owner: string().describe("Owner's account id."),
+
+  admins: array(string())
+    .optional()
+    .describe("List of pot admins' account ids."),
+
+  chef: string().optional().describe("Chef's account id."),
+  pot_name: string().describe("Pot name."),
+  pot_description: string().describe("Pot description."),
+  max_projects: number().describe("Maximum number of approved projects."),
+  application_start_ms: number().describe("Application start timestamp."),
+  application_end_ms: number().describe("Application end timestamp."),
+  public_round_start_ms: number().describe("Matching round start timestamp."),
+  public_round_end_ms: number().describe("Matching round end timestamp."),
+
+  min_matching_pool_donation_amount: string()
+    .optional()
+    .describe("Minimum donation amount."),
+
+  cooldown_period_ms: number()
+    .optional()
+    .describe("Cooldown period in milliseconds."),
+
+  registry_provider: string()
+    .optional()
+    .describe("Registry provider's account id."),
+
+  sybil_wrapper_provider: string()
+    .optional()
+    .describe("Sybil wrapper provider's account id."),
+
+  referral_fee_matching_pool_basis_points: number().describe(
+    "Matching pool referral fee in basis points.",
+  ),
+
+  referral_fee_public_round_basis_points: number().describe(
+    "Public round referral fee in basis points.",
+  ),
+
+  chef_fee_basis_points: number().describe("Chef fee in basis points."),
+});
+
+export type PotDeploymentInputs = FromSchema<typeof potDeploymentSchema>;
