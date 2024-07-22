5c41b0c096adc0e512de7c4835c05e81c33cd533
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-07-22 21:50:56 +0400

    wip: Refactor user pots tab

--- src/pages/user/[userId]/pots/page.tsx
+++ src/pages/user/[userId]/pots/page.tsx
@@ -1,35 +1,28 @@
 /* eslint-disable @next/next/no-img-element */
 
-import { useEffect, useMemo, useState } from "react";
+import { useRouter } from "next/router";
 
-import { useParams } from "next/navigation";
-
-import { getAccountPotApplications } from "@/common/api/potlock/account";
+import { potlock } from "@/common/api/potlock";
 import PotCard from "@/modules/pot/components/PotCard";
 
 const PotsSubPage = () => {
-  const { userId } = useParams<{ userId: string }>();
+  const router = useRouter();
+  const { userId: userIdPathParam } = router.query;
 
-  // useAccountPotApplications is not working
-  // const foo = useAccountPotApplications({ accountId: userId });
+  const userId =
+    typeof userIdPathParam === "string" ? userIdPathParam : undefined;
 
-  const [potApplications, setPotApplications] = useState<any[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const { data: paginatedPotApplications, isLoading } =
+    potlock.useAccountPotApplications({ accountId: userId });
 
-  useEffect(() => {
-    (async () => {
-      const res = await getAccountPotApplications({ accountId: userId });
-
-      setPotApplications(res);
-      setIsLoading(false);
-    })();
-  }, [userId]);
+  const potApplications = paginatedPotApplications?.results ?? [];
 
   const NoResults = () => (
     <div className="flex flex-col-reverse items-center justify-between rounded-[12px] bg-[#f6f5f3] px-[24px] py-[16px] md:flex-col md:px-[105px] md:py-[68px]">
       <p className="font-italic font-500 mb-4 max-w-[290px] text-center font-lora text-[16px] text-[#292929] md:text-[22px]">
         This project has not participated in any pots yet.
       </p>
+
       <img
         className="w-[50%]"
         src="https://ipfs.near.social/ipfs/bafkreibcjfkv5v2e2n3iuaaaxearps2xgjpc6jmuam5tpouvi76tvfr2de"
@@ -38,19 +31,14 @@ const PotsSubPage = () => {
     </div>
   );
 
-  const potCards = useMemo(() => {
-    if (potApplications) {
-      return potApplications.map((application) => (
-        <PotCard key={application.pot.id} pot={application.pot} />
-      ));
-    }
-  }, [potApplications]);
-
   return (
     <div className="mb-18 flex w-full flex-col">
       <div className="mx-auto grid w-fit grid-cols-1 gap-[31px] pb-[32px] pt-[2px] lg:w-full lg:grid-cols-2 xl:grid-cols-3">
-        {potCards}
+        {potApplications.map(({ id, pot }) => (
+          <PotCard key={id} {...{ pot }} />
+        ))}
       </div>
+
       {potApplications.length === 0 && !isLoading && <NoResults />}
     </div>
   );
