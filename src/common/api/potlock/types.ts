63ee2e2884c156ab1d5d569545ed15e095f9ab59
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-07-24 02:22:17 +0400

    Fix type errors

--- src/common/api/potlock/types.ts
+++ src/common/api/potlock/types.ts
@@ -2,7 +2,7 @@ import { Pot } from "./generated/client";
 
 export * from "./generated/client";
 
-export type PotId = Pot["id"];
+export type PotId = Pot["account"];
 
 export interface ByPotId {
   potId: PotId;
