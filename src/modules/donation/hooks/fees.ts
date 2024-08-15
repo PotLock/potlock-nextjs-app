7d6181b7ac131821dff68915f2aba64f5de5ac41
Harjaap Dhillon <harvydhillon16@gmail.com>
2024-08-15 19:18:54 +0530

    changes

--- src/modules/donation/hooks/fees.ts
+++ src/modules/donation/hooks/fees.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
 import { Pot, potlock } from "@/common/api/potlock";
 import { TOTAL_FEE_BASIS_POINTS } from "@/modules/core/constants";
 
--- src/modules/donation/models/index.ts
+++ src/modules/donation/models/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
 import { createModel } from "@rematch/core";
 
 import { walletApi } from "@/common/contracts";
