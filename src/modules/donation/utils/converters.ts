63a7ad5f568ca5d3f54bb73455cdf2c23ca2065a
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-08-21 13:39:19 +0000

    chore: Use basic points total explicitly

--- src/modules/donation/utils/converters.ts
+++ src/modules/donation/utils/converters.ts
@@ -1,7 +1,7 @@
 import { TOTAL_FEE_BASIS_POINTS } from "@/modules/core/constants";
 
 export const donationFeeBasisPointsToPercents = (basisPoints: number) =>
-  basisPoints / 100;
+  basisPoints / (TOTAL_FEE_BASIS_POINTS / 100);
 
 export const donationFeePercentsToBasisPoints = (percent: number) =>
   percent * (TOTAL_FEE_BASIS_POINTS / 100);
