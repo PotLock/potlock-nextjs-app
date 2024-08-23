c422d4f48dee84a8acb5c5da949be2a11907bf2b
Wenderson Pires <wendersonpdas@gmail.com>
2024-08-23 13:29:54 -0300

    projects tab page done

--- src/pages/pot/[potId]/projects/index.tsx
+++ src/pages/pot/[potId]/projects/index.tsx
@@ -4,8 +4,14 @@ import { useRouter } from "next/router";
 
 import { Application } from "@/common/contracts/potlock/interfaces/pot.interfaces";
 import * as potServices from "@/common/contracts/potlock/pot";
+import { InfiniteScroll, SearchBar } from "@/common/ui/components";
 import { PotLayout } from "@/modules/pot";
+import { Profile } from "@/modules/profile/models";
+import { ProjectCard } from "@/modules/project";
+import { useTypedSelector } from "@/store";
 // import { usePotApplications } from "@/common/api/potlock/hooks";
+
+const MAXIMUM_CARDS_PER_INDEX = 9;
 
 const ProjectsTab = () => {
   const router = useRouter();
@@ -14,6 +20,12 @@ const ProjectsTab = () => {
   // NOTE: not working
   // const potProjectsData = usePotApplications(query.potId);
   const [potProjects, setPotProjects] = useState<Application[]>([]);
+  const [filteredPotProjects, setFilteredPotProjects] = useState<Application[]>(
+    [],
+  );
+  const [search, setSearch] = useState("");
+  const registrationsProfile = useTypedSelector((state) => state.profiles);
+  const [index, setIndex] = useState(1);
 
   useEffect(() => {
     if (query.potId) {
@@ -23,19 +35,80 @@ const ProjectsTab = () => {
         });
 
         // Set only the approved ones
-        // TODO: Uncomment
-        // setPotProjects(_projects.filter(application => application.status === "Approved"));
-        setPotProjects(_projects);
+        setPotProjects(
+          _projects.filter((application) => application.status === "Approved"),
+        );
+        setFilteredPotProjects(_projects);
       })();
     }
   }, [query.potId]);
 
-  console.log(potProjects);
+  // Handle Search
+  useEffect(() => {
+    const handleSearch = (application: Application, profile: Profile) => {
+      if (search === "") return true;
+      const { project_id: projectId } = application;
+      const { socialData, tags, team } = profile || {};
+      // registration fields to search in
+      const fields = [
+        projectId,
+        socialData?.description,
+        socialData?.name,
+        tags?.join(" "),
+        team?.join(" "),
+      ];
+
+      return fields.some((item) => (item || "").toLowerCase().includes(search));
+    };
+
+    if (search) {
+      const filteredApplicationProjects = potProjects.filter(
+        (applicationProject) => {
+          const profile =
+            registrationsProfile[applicationProject.project_id] || {};
+
+          return handleSearch(applicationProject, profile);
+        },
+      );
+
+      setFilteredPotProjects(filteredApplicationProjects);
+    }
+  }, [search, potProjects, registrationsProfile]);
 
   return (
-    <main className="flex flex-col">
-      <p>Projects Page</p>
-    </main>
+    <div className="md:py-12 flex w-full flex-col py-10">
+      <div className="flex w-full flex-col gap-5">
+        <div className="font-600 text-[18px] leading-6 tracking-[1.12px] text-[#292929]">
+          All Projects
+          <span className="ml-2">{potProjects.length}</span>
+        </div>
+        <div className="flex w-full items-center gap-4">
+          <SearchBar
+            placeholder="Search projects"
+            onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          />
+        </div>
+      </div>
+      {filteredPotProjects.length ? (
+        <InfiniteScroll
+          className="p-0.5"
+          items={filteredPotProjects}
+          index={index}
+          setIndex={setIndex}
+          size={MAXIMUM_CARDS_PER_INDEX}
+          renderItem={(application: Application) => (
+            <ProjectCard
+              projectId={application.project_id}
+              key={application.project_id}
+            />
+          )}
+        />
+      ) : (
+        <div style={{ alignSelf: "flex-start", margin: "24px 0px" }}>
+          No projects
+        </div>
+      )}
+    </div>
   );
 };
 
