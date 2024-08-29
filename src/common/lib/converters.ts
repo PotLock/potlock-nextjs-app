2391d8bb2170441f5067a6de3611d1f73c8f48bc
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-08-29 13:56:52 +0000

    Fix bignum conversion

--- src/common/lib/converters.ts
+++ src/common/lib/converters.ts
@@ -14,7 +14,7 @@ export const yoctosToNear = (amountYoctos: string, abbreviate?: boolean) => {
 };
 
 export const bigNumFromString = (amount: string, decimals: number) =>
-  Big(amount).mul(Big(10).pow(decimals));
+  Big(amount).div(Big(10).pow(decimals));
 
 export const bigStringToFloat = (amount: string, decimals: number) =>
   parseFloat(bigNumFromString(amount, decimals).toFixed(2));
