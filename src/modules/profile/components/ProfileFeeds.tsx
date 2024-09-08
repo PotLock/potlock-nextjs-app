35526c87fb4920ea63e4cd5aa8841d6ee301afcd
macbook <ebubeagwaze@gmail.com>
2024-09-08 17:10:32 +0100

    handled responsiveness

--- src/modules/profile/components/ProfileFeeds.tsx
+++ src/modules/profile/components/ProfileFeeds.tsx
@@ -2,6 +2,8 @@
 
 import React, { useEffect, useRef, useState } from "react";
 
+import InfiniteScrollWrapper from "react-infinite-scroll-component";
+
 import { fetchAccountFeedPosts } from "@/common/api/near-social";
 import {
   IndexPostResultItem,
@@ -25,7 +27,6 @@ export const ProfileFeeds: React.FC<ProfileFeedsProps> = ({ accountId }) => {
   }, [accountId]);
 
   const loadMorePosts = async () => {
-    if (isLoading) return; // Prevent multiple calls while loading
     setIsLoading(true);
 
     const fetchedPosts = await fetchAccountFeedPosts({
@@ -33,36 +34,12 @@ export const ProfileFeeds: React.FC<ProfileFeedsProps> = ({ accountId }) => {
       offset,
     });
 
-    // Create a Set of existing post block heights to filter out duplicates
-    const existingPostIds = new Set(posts.map((post) => post.blockHeight));
-
-    // Filter out previously fetched posts
-    const newPosts = fetchedPosts.filter(
-      (post) => !existingPostIds.has(post.blockHeight),
-    );
+    const newPosts = fetchedPosts.slice(offset - 20);
 
     setPosts((prevPosts) => [...prevPosts, ...newPosts]);
     setOffset((prevOffset) => prevOffset + 20);
     setIsLoading(false);
   };
-
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) {
-        loadMorePosts();
-      }
-    });
-
-    if (loadingRef.current) {
-      observer.observe(loadingRef.current);
-    }
-
-    return () => {
-      if (loadingRef.current) {
-        observer.unobserve(loadingRef.current);
-      }
-    };
-  }, [loadingRef.current]);
 
   const NoResults = () => (
     <div className="md:flex-col md:px-[105px] md:py-[68px] flex flex-col-reverse items-center justify-between rounded-[12px] bg-[#f6f5f3] px-[24px] py-[16px]">
@@ -80,14 +57,22 @@ export const ProfileFeeds: React.FC<ProfileFeedsProps> = ({ accountId }) => {
 
   return (
     <div className="my-8 h-full max-h-80 w-full">
-      {posts.map((post) => (
-        <FeedCard key={post.blockHeight} post={post} />
-      ))}
-      {posts.length > 1 && (
-        <div ref={loadingRef} className="mt-4 min-h-12 text-center">
-          {isLoading ? "Loading more posts..." : ""}
-        </div>
-      )}
+      <InfiniteScrollWrapper
+        className="space-y-4"
+        dataLength={40}
+        scrollThreshold={1}
+        hasMore={2 < 5}
+        next={loadMorePosts}
+        loader={
+          <div ref={loadingRef} className="mt-4 min-h-12 text-center">
+            <div className="">Loading...</div>
+          </div>
+        }
+      >
+        {posts.map((post) => (
+          <FeedCard key={post.blockHeight} post={post} />
+        ))}
+      </InfiniteScrollWrapper>
       {posts.length === 0 && <NoResults />}
     </div>
   );
