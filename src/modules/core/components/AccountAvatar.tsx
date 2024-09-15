c0a9b5e4cd35f94f5806f91ff8655fed772f8a25
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-09-15 18:27:36 +0000

    Fix avatars

--- src/modules/core/components/AccountAvatar.tsx
+++ src/modules/core/components/AccountAvatar.tsx
@@ -1,11 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 
-import { useState } from "react";
+import { useMemo } from "react";
 
 import useProfileData from "@/modules/profile/hooks/useProfileData";
-
-const NO_IMAGE =
-  "https://ipfs.near.social/ipfs/bafkreiccpup6f2kihv7bhlkfi4omttbjpawnsns667gti7jbhqvdnj4vsm";
 
 export const AccountAvatar = ({
   accountId,
@@ -15,19 +12,21 @@ export const AccountAvatar = ({
   className?: string;
 }) => {
   const profileInfo = useProfileData(accountId);
-  const [hasError, setHasError] = useState(false);
 
-  const imageSrc =
-    typeof profileInfo.profile?.image === "string"
-      ? profileInfo.profile.image
-      : profileInfo.profile?.image?.url ?? profileInfo.profileImages.image;
+  const imageSrc = useMemo(
+    () =>
+      typeof profileInfo.profile?.image === "string"
+        ? profileInfo.profile.image
+        : profileInfo.profile?.image?.url ?? profileInfo.profileImages.image,
+
+    [profileInfo.profile?.image, profileInfo.profileImages.image],
+  );
 
   return (
     <img
       alt="avatar"
       className={`h-[12px] w-[12px] rounded-[50%] bg-white ${className}`}
-      src={hasError ? NO_IMAGE : imageSrc}
-      onError={() => setHasError(true)}
+      src={imageSrc}
     />
   );
 };
