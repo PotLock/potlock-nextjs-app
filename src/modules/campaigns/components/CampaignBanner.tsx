ba77273d134a8605c8a3f4113b09ffd41ce32268
macbook <ebubeagwaze@gmail.com>
2024-09-27 06:03:48 +0100

    init project

---
+++ src/modules/campaigns/components/CampaignBanner.tsx
@@ -0,0 +1,7 @@
+export const CampaignBanner = () => {
+  return (
+    <div className="relative flex min-h-[400px] w-full flex-col items-start justify-center overflow-hidden bg-hero">
+      <h1 className="font-500 font-lora tracking-[1.12px]">Fund Your Ideas</h1>
+    </div>
+  );
+};
---
+++ src/pages/projects.tsx
@@ -0,0 +1,10 @@
+import { PageWithBanner } from "@/common/ui/components";
+import { CampaignBanner } from "@/modules/campaigns/components/CampaignBanner";
+
+export const Projects = () => {
+  return (
+    <PageWithBanner>
+      <CampaignBanner />
+    </PageWithBanner>
+  );
+};
