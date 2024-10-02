97b27886211a838242eefcde27d34120c253532e
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-10-02 11:08:12 +0000

    Implement detailed breakdown for group donations

--- src/common/ui/components/atoms/accordion.tsx
+++ src/common/ui/components/atoms/accordion.tsx
@@ -32,7 +32,8 @@ export const AccordionTrigger = forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline",
+        "flex flex-1 items-center justify-between py-4 font-medium transition-all",
+        "hover:underline [&[data-state=open]>svg]:rotate-180",
         className,
       )}
       {...props}
