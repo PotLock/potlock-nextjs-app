051ab03e0f32566781a17f02bcf144f8880a7cb0
Jiku Godwill Nsanwi <jikugodwill@yahoo.com>
2024-10-14 15:57:50 +0100

    Refactor LeaderboardPage component to add activities tab and display activity cards

--- src/app/donors/page.tsx
+++ src/app/donors/page.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 
+import Image from "next/image";
+
 import { Button, SearchBar, ToggleGroup } from "@/common/ui/components";
 
 import { LeaderboardCard } from "../_components/LeaderboardCard";
@@ -12,6 +14,17 @@ interface Participant {
   name: string;
   amount: number;
   amountUsd: number;
+}
+
+interface Activity {
+  sender: string;
+  senderImage: string;
+  amount: number;
+  amountUsd: number;
+  currency: string;
+  receiver: string;
+  receiverImage: string;
+  timestamp: number;
 }
 
 const topDonors: Participant[] = [
@@ -86,6 +99,73 @@ const topSponsors: Participant[] = [
   },
 ];
 
+const ACTIVITY: Activity[] = [
+  {
+    sender: "nearcollective.near",
+    senderImage: "/placeholder.svg?height=40&width=40",
+    amount: 1000,
+    amountUsd: 1000,
+    currency: "NEAR",
+    receiver: "creativesportfolio.near",
+    receiverImage: "/placeholder.svg?height=40&width=40",
+    timestamp: Date.now() - 1000 * 60 * 60 * 24 * 2,
+  },
+  {
+    sender: "nf-payments.near",
+    senderImage: "/placeholder.svg?height=40&width=40",
+    amount: 1000,
+    amountUsd: 1000,
+    currency: "NEAR",
+    receiver: "mike.near",
+    receiverImage: "/placeholder.svg?height=40&width=40",
+    timestamp: Date.now() - 1000 * 60 * 60 * 24 * 2,
+  },
+  {
+    sender: "creatives.potlock.near",
+    senderImage: "/placeholder.svg?height=40&width=40",
+    amount: 1000,
+    amountUsd: 1000,
+    currency: "NEAR",
+    receiver: "mike.near",
+    receiverImage: "/placeholder.svg?height=40&width=40",
+    timestamp: Date.now() - 1000 * 60 * 60 * 24 * 2,
+  },
+  {
+    sender: "nearcollective.near",
+    senderImage: "/placeholder.svg?height=40&width=40",
+    amount: 1000,
+    amountUsd: 1000,
+    currency: "NEAR",
+    receiver: "creativesportfolio.near",
+    receiverImage: "/placeholder.svg?height=40&width=40",
+    timestamp: Date.now() - 1000 * 60 * 60 * 24 * 2,
+  },
+  {
+    sender: "nf-payments.near",
+    senderImage: "/placeholder.svg?height=40&width=40",
+    amount: 1000,
+    amountUsd: 1000,
+    currency: "NEAR",
+    receiver: "mike.near",
+    receiverImage: "/placeholder.svg?height=40&width=40",
+    timestamp: Date.now() - 1000 * 60 * 60 * 24 * 2,
+  },
+];
+
+const timeAgo = (timestamp: number) => {
+  const now = Date.now();
+  const diff = now - timestamp;
+  if (diff < 1000 * 60) {
+    return `${Math.floor(diff / 1000)}s ago`;
+  } else if (diff < 1000 * 60 * 60) {
+    return `${Math.floor(diff / (1000 * 60))}mns ago`;
+  } else if (diff < 1000 * 60 * 60 * 24) {
+    return `${Math.floor(diff / (1000 * 60 * 60))}hrs ago`;
+  } else {
+    return `${Math.floor(diff / (1000 * 60 * 60 * 24))}days ago`;
+  }
+};
+
 export default function LeaderboardPage() {
   const [searchTerm, setSearchTerm] = useState("");
   const [timeFilter, setTimeFilter] = useState("All time");
@@ -101,14 +181,14 @@ export default function LeaderboardPage() {
     type: "donor" | "sponsor",
   ) => (
     <>
-      <div className="flex w-full justify-between py-10 sm:w-auto">
+      <div className="flex w-full flex-col gap-x-14 gap-y-4 py-10 sm:w-auto md:flex-row md:justify-between">
         <SearchBar
-          className="max-w-md text-gray-400"
+          className="w-85% max-w-xs flex-1 text-gray-400"
           placeholder={`Search projects`}
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <div className="flex flex-wrap gap-2">
+        <div className="flex w-fit gap-1">
           {["All time", "1Y", "30D", "1W", "1D"].map((filter) => (
             <Button
               key={filter}
@@ -116,14 +196,14 @@ export default function LeaderboardPage() {
                 timeFilter === filter ? "sec-brand-filled" : "standard-outline"
               }
               onClick={() => setTimeFilter(filter)}
-              className="text-sm"
+              className="py-0.375 px-3 text-sm"
             >
               {filter}
             </Button>
           ))}
         </div>
       </div>
-      <div className="mb-8 grid w-full grid-cols-3 gap-4 overflow-x-scroll">
+      <div className="max-w-1000px min-w-lg mb-8 grid w-full grid-cols-3 gap-4 overflow-x-scroll">
         {participants.slice(0, 3).map((participant) => (
           <LeaderboardCard
             key={participant.rank}
@@ -242,12 +322,81 @@ export default function LeaderboardPage() {
         </div>
       </ToggleGroup>
       <div className="w-full flex-nowrap py-10">
-        <div>
+        <div className="md:w-80% mx-auto w-full">
           {selectedTab === "activities" ? (
-            <div>
-              <h1 className="text-center font-lora text-3xl font-bold md:text-5xl">
+            <div className="w-full">
+              <h1 className="font-lora text-3xl font-bold md:text-5xl">
                 All Activities
               </h1>
+              <div className="flex w-full flex-col justify-between gap-x-14 gap-y-4 py-10 sm:w-auto md:flex-row">
+                <SearchBar
+                  className="w-85% max-w-xs text-gray-400 md:w-auto"
+                  placeholder={`Search projects`}
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <div className="flex w-fit gap-1">
+                  {["All time", "1Y", "30D", "1W", "1D"].map((filter) => (
+                    <Button
+                      key={filter}
+                      variant={
+                        timeFilter === filter
+                          ? "sec-brand-filled"
+                          : "standard-outline"
+                      }
+                      onClick={() => setTimeFilter(filter)}
+                      className="py-0.375 px-3 text-sm"
+                    >
+                      {filter}
+                    </Button>
+                  ))}
+                </div>
+              </div>
+              <div className="overflow-x-auto bg-white">
+                {ACTIVITY.map((activity, index) => (
+                  <div
+                    key={index}
+                    className="border-gray-#ebebeb my-4 flex flex-wrap items-center justify-between rounded-xl border py-4"
+                  >
+                    <div className="w-224px flex items-center gap-2 overflow-x-scroll px-4 text-sm font-medium text-gray-900">
+                      <Image
+                        src={activity.senderImage}
+                        className="h-10 w-10 rounded-full"
+                        alt="sender image"
+                        width={10}
+                        height={10}
+                      />
+                      <h1>{activity.sender}</h1>
+                    </div>
+                    <div className="flex gap-2 px-4 align-middle text-sm text-gray-600">
+                      Donated
+                    </div>
+                    <div className="w-140px flex items-center">
+                      <div className="ml-4">
+                        <div className="text-sm font-medium text-gray-900">
+                          {activity.amount} {activity.currency}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex items-center gap-2 px-4 text-sm text-gray-600">
+                      to
+                    </div>
+                    <div className="w-224px flex items-center gap-2 overflow-x-scroll px-4 text-sm font-medium text-gray-900">
+                      <Image
+                        src={activity.receiverImage}
+                        className="h-10 w-10 rounded-full"
+                        alt="receiver image"
+                        width={10}
+                        height={10}
+                      />
+                      <h1>{activity.receiver}</h1>
+                    </div>
+                    <div className="w-120px flex items-center gap-2 whitespace-nowrap px-4 text-sm text-gray-600">
+                      {timeAgo(activity.timestamp)}
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
           ) : (
             ""
