e376d28196ca6c1c69fcfe20dd16b3cabb1f19b7
Ebube <ebubeagwaze@gmail.com>
2024-10-15 16:58:54 +0100

    es lint

--- src/modules/campaigns/hooks/forms.ts
+++ src/modules/campaigns/hooks/forms.ts
@@ -12,45 +12,45 @@ import { floatToYoctoNear } from "@/common/lib";
 import { campaignFormSchema } from "../models/schema";
 
 export const useCampaignForm = () => {
-    type Values = FromSchema<typeof campaignFormSchema>;
+  type Values = FromSchema<typeof campaignFormSchema>;
 
-    const self = useForm<Values>({
-        resolver: zodResolver(campaignFormSchema),
-        mode: "onChange",
-        resetOptions: { keepDirtyValues: true },
+  const self = useForm<Values>({
+    resolver: zodResolver(campaignFormSchema),
+    mode: "onChange",
+    resetOptions: { keepDirtyValues: true },
+  });
+
+  const values = useWatch(self);
+
+  const onSubmit: SubmitHandler<Values> = useCallback((values) => {
+    campaign.create_campaign({
+      args: {
+        ...values,
+        target_amount: floatToYoctoNear(values.target_amount) as any,
+        start_ms: Temporal.Instant.from(values.start_ms.toString() + "Z")
+          .epochMilliseconds,
+        // ...(values.end_ms
+        //     ? { end_ms: localeStringToTimestampMs(values.end_ms.toString()) }
+        //     : {}),
+        owner: walletApi.accountId as string,
+      },
     });
+  }, []);
 
-    const values = useWatch(self);
+  const onChange = (field: keyof Values, value: string) => {
+    self.setValue(field, value);
+  };
 
-    const onSubmit: SubmitHandler<Values> = useCallback((values) => {
-        campaign.create_campaign({
-            args: {
-                ...values,
-                target_amount: floatToYoctoNear(values.target_amount) as any,
-                start_ms: Temporal.Instant.from(values.start_ms.toString() + "Z")
-                    .epochMilliseconds,
-                // ...(values.end_ms
-                //     ? { end_ms: localeStringToTimestampMs(values.end_ms.toString()) }
-                //     : {}),
-                owner: walletApi.accountId as string,
-            },
-        });
-    }, []);
-
-    const onChange = (field: keyof Values, value: string) => {
-        self.setValue(field, value);
-    };
-
-    return {
-        form: {
-            ...self,
-            formState: {
-                ...self.formState,
-                error: { ...self.formState.errors },
-            },
-        },
-        onSubmit,
-        values,
-        onChange,
-    };
+  return {
+    form: {
+      ...self,
+      formState: {
+        ...self.formState,
+        error: { ...self.formState.errors },
+      },
+    },
+    onSubmit,
+    values,
+    onChange,
+  };
 };
