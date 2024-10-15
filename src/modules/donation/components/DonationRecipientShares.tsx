c06c1d537405e216b3cd6b3c18c91c4afe138471
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-10-15 01:10:07 +0000

    wip

--- src/modules/donation/components/DonationRecipientShares.tsx
+++ src/modules/donation/components/DonationRecipientShares.tsx
@@ -4,7 +4,7 @@ import { potlock } from "@/common/api/potlock";
 import { NearIcon } from "@/common/assets/svgs";
 import { FormField } from "@/common/ui/components";
 import { CheckboxField, TextField } from "@/common/ui/form-fields";
-import { AccountOption } from "@/modules/core";
+import { AccountOption, RuntimeErrorAlert } from "@/modules/core";
 
 import { DONATION_INSUFFICIENT_BALANCE_ERROR } from "../constants";
 import {
@@ -31,9 +31,7 @@ export const DonationRecipientShares: React.FC<
     potlock.usePotApplications({ potId, page_size: 100 });
 
   const { data: listRegistrations = [], error: listRegistrationsError } =
-    potlock.useListRegistrations({
-      listId,
-    });
+    potlock.useListRegistrations({ listId });
 
   const handleEvenShareAllocation = useDonationEvenShareAllocation({
     form,
@@ -48,13 +46,19 @@ export const DonationRecipientShares: React.FC<
       return {
         heading: "Pot donation",
         title: "Unable to load recipients' data!",
-        message: potApplicationsError.message,
+
+        message: potApplicationsError.code
+          ? `${potApplicationsError.code}: ${potApplicationsError.message}`
+          : potApplicationsError.message,
       };
     } else if (listRegistrationsError) {
       return {
         heading: "List donation",
         title: "Unable to load recipients' data!",
-        message: listRegistrationsError.message,
+
+        message: listRegistrationsError.code
+          ? `${listRegistrationsError.code}: ${listRegistrationsError.message}`
+          : listRegistrationsError.message,
       };
     } else return null;
   }, [listRegistrationsError, potApplicationsError]);
@@ -64,55 +68,60 @@ export const DonationRecipientShares: React.FC<
       "registrant" in entry ? entry.registrant.id : entry.applicant.id,
   );
 
-  console.log("recipientCandidateIds", recipientCandidateIds);
-
-  return recipientCandidateIds.map((accountId) => (
-    <AccountOption
-      highlightOnHover
-      key={accountId}
-      {...{ accountId }}
-      secondaryAction={
-        <FormField
-          name="groupAllocationPlan"
-          control={form.control}
-          render={({ field: { value = [], ...field } }) =>
-            groupAllocationStrategy === "evenly" ? (
-              <CheckboxField
-                {...field}
-                checked={value.some(
-                  (recipient) =>
-                    recipient.account_id === accountId &&
-                    recipient.amount !== undefined,
-                )}
-                onCheckedChange={handleEvenShareAllocation({
-                  accountId,
-                })}
-              />
-            ) : (
-              <TextField
-                {...field}
-                type="number"
-                placeholder="0.00"
-                min={0}
-                max={balanceFloat ?? undefined}
-                step={0.01}
-                defaultValue={
-                  value.find((recipient) => recipient.account_id === accountId)
-                    ?.amount
-                }
-                onChange={handleManualShareAllocation({ accountId })}
-                appendix={<NearIcon width={24} height={24} />}
-                customErrorMessage={
-                  isBalanceSufficient
-                    ? null
-                    : DONATION_INSUFFICIENT_BALANCE_ERROR
-                }
-                classNames={{ fieldRoot: "w-32" }}
-              />
-            )
-          }
-        />
-      }
-    />
-  ));
+  return errorDetails ? (
+    <div className="w-full p-4">
+      <RuntimeErrorAlert {...errorDetails} />
+    </div>
+  ) : (
+    recipientCandidateIds.map((accountId) => (
+      <AccountOption
+        highlightOnHover
+        key={accountId}
+        {...{ accountId }}
+        secondaryAction={
+          <FormField
+            name="groupAllocationPlan"
+            control={form.control}
+            render={({ field: { value = [], ...field } }) =>
+              groupAllocationStrategy === "evenly" ? (
+                <CheckboxField
+                  {...field}
+                  checked={value.some(
+                    (recipient) =>
+                      recipient.account_id === accountId &&
+                      recipient.amount !== undefined,
+                  )}
+                  onCheckedChange={handleEvenShareAllocation({
+                    accountId,
+                  })}
+                />
+              ) : (
+                <TextField
+                  {...field}
+                  type="number"
+                  placeholder="0.00"
+                  min={0}
+                  max={balanceFloat ?? undefined}
+                  step={0.01}
+                  defaultValue={
+                    value.find(
+                      (recipient) => recipient.account_id === accountId,
+                    )?.amount
+                  }
+                  onChange={handleManualShareAllocation({ accountId })}
+                  appendix={<NearIcon width={24} height={24} />}
+                  customErrorMessage={
+                    isBalanceSufficient
+                      ? null
+                      : DONATION_INSUFFICIENT_BALANCE_ERROR
+                  }
+                  classNames={{ fieldRoot: "w-32" }}
+                />
+              )
+            }
+          />
+        }
+      />
+    ))
+  );
 };
