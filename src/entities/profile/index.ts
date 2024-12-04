6956a9e92bc78c6611eb542e8bd4054fcdc989ee
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-12-04 04:39:53 +0000

    Apply performance optimizations

--- src/entities/profile/index.ts
+++ src/entities/profile/index.ts
@@ -1,5 +1,5 @@
 export * from "./components/FeedCard";
 export * from "./components/ProfileLayout";
-export { profilesModel, navModel } from "./models";
+export { navModel } from "./models";
 export { useProfileData } from "./hooks/data";
 export * from "./utils/validation";
