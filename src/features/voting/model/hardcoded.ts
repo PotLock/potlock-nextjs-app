d35ca0f95e3f9f819a9ba949830a7b2af5b8bde8
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-12-06 14:28:07 +0000

    wip

--- src/features/voting/model/hardcoded.ts
+++ src/features/voting/model/hardcoded.ts
@@ -2,18 +2,7 @@ import { METAPOOL_LIQUID_STAKING_CONTRACT_ACCOUNT_ID } from "@/common/_config";
 
 import { VotingMechanismConfig } from "../types";
 
-// TODO: Convert into publicly available configurable JSON ( Pots V2 milestone )
-/**
- * - Human-verified Users: Votes are weighted at 10% for verified users [KYC].
- *
- * - mpDAO Governance Participants: Users with at least 10,000 votes in mpDAO governance
- *  receive an additional 25% vote weight. Those with 25,000 votes gain another 25%.
- *
- * - stNEAR Stakeholders: Users with 2 stNEAR staked in Meta Pool receive a 10% boost,
- *  and those with 10 stNEAR staked gain a 30% vote weight increase.
- *
- * https://docs.google.com/document/d/1P5iSBBSuh7nep29r7N3S-g4Y1bDbF4xLU_3v7XHmJR8
- */
+// TODO: Convert into publicly available configurable JSON ( Pots V2 milestone ) and validate with Zod
 export const VOTING_MECHANISM_CONFIG_MPDAO: VotingMechanismConfig = {
   basicWeight: 1,
   stakingContractAccountId: METAPOOL_LIQUID_STAKING_CONTRACT_ACCOUNT_ID,
