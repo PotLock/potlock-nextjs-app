7cc005e72307f061a19f86fbbf6664261369a38a
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-12-06 16:11:51 +0000

    hotfix: Remove mocks

--- src/pages/pot/[potId]/votes.tsx
+++ src/pages/pot/[potId]/votes.tsx
@@ -39,62 +39,13 @@ interface Project {
 }
 
 const VOTING_DUMMY_PROJECTS: Project[] = [
-  {
-    id: "1",
-    name: "Mike.near",
-    votes: 2000,
-    voted: false,
-    imageUrl: "https://picsum.photos/200/200/?blur",
-  },
-  {
-    id: "2",
-    name: "Mike.near",
-    votes: 2000,
-    voted: false,
-    imageUrl: "https://picsum.photos/200/200/?blur",
-  },
-  {
-    id: "3",
-    name: "Mike.near",
-    votes: 2000,
-    voted: false,
-    imageUrl: "https://picsum.photos/200/200/?blur",
-  },
-  {
-    id: "4",
-    name: "Mike.near",
-    votes: 2000,
-    voted: false,
-    imageUrl: "https://picsum.photos/200/200/?blur",
-  },
-  {
-    id: "5",
-    name: "Mike.near",
-    votes: 2000,
-    voted: false,
-    imageUrl: "https://picsum.photos/200/200/?blur",
-  },
-  {
-    id: "6",
-    name: "Mike.near",
-    votes: 2000,
-    voted: false,
-    imageUrl: "https://picsum.photos/200/200/?blur",
-  },
-  {
-    id: "7",
-    name: "Mike.near",
-    votes: 2000,
-    voted: false,
-    imageUrl: "https://picsum.photos/200/200/?blur",
-  },
-  {
-    id: "8",
-    name: "Mike.near",
-    votes: 2000,
-    voted: false,
-    imageUrl: "https://picsum.photos/200/200/?blur",
-  },
+  // {
+  //   id: "1",
+  //   name: "Mike.near",
+  //   votes: 2000,
+  //   voted: false,
+  //   imageUrl: "https://picsum.photos/200/200/?blur",
+  // },
 ];
 
 type FilterType = "all" | "voted" | "pending";
