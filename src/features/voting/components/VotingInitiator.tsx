25fdd4a2b16a0edc7f9256de7ea51d1c290960ec
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-12-09 07:11:12 +0000

    wip

---
+++ src/features/voting/components/VotingInitiator.tsx
@@ -0,0 +1,7 @@
+import { ByPotId } from "@/common/api/indexer";
+
+export type VotingInitiatorProps = ByPotId & {};
+
+export const VotingInitiator: React.FC<VotingInitiatorProps> = ({ potId }) => {
+  return null;
+};
