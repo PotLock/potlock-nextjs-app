959501253580a6336fff8b3cd1ad1fd727df4bfe
Ebube <ebubeagwaze@gmail.com>
2024-12-09 10:00:08 +0100

    eslint fix

--- src/entities/pot/hooks/usePotPayoutLookup.tsx
+++ src/entities/pot/hooks/usePotPayoutLookup.tsx
@@ -5,36 +5,36 @@ import { ByPotId, indexer } from "@/common/api/indexer";
 export type PotPayoutLookupParams = ByPotId & {};
 
 export const usePotPayoutLookup = ({ potId }: PotPayoutLookupParams) => {
-    const [pageNumber, setPageNumber] = useState(1);
-    const [searchTerm, setSearchTerm] = useState<string | undefined>("");
+  const [pageNumber, setPageNumber] = useState(1);
+  const [searchTerm, setSearchTerm] = useState<string | undefined>("");
 
-    const {
-        data: potPayouts,
-        isLoading,
-        error,
-    } = indexer.usePotPayout({
-        potId,
-        page_size: 10,
-        page: pageNumber,
-        search: searchTerm,
-    });
+  const {
+    data: potPayouts,
+    isLoading,
+    error,
+  } = indexer.usePotPayout({
+    potId,
+    page_size: 10,
+    page: pageNumber,
+    search: searchTerm,
+  });
 
-    const results = useMemo(() => {
-        return potPayouts?.results?.map((payout) => ({
-            project_id: payout?.recipient?.id,
-            id: payout?.id,
-            amount: payout?.amount,
-        }));
-    }, [potPayouts]);
+  const results = useMemo(() => {
+    return potPayouts?.results?.map((payout) => ({
+      project_id: payout?.recipient?.id,
+      id: payout?.id,
+      amount: payout?.amount,
+    }));
+  }, [potPayouts]);
 
-    return {
-        payoutsError: error,
-        payouts: results,
-        isPayoutsPending: isLoading,
-        setPayoutSearchTerm: setSearchTerm,
-        setPayoutPageNumber: setPageNumber,
-        payoutPageNumber: pageNumber,
-        payoutSearchTerm: searchTerm,
-        totalPayoutCount: potPayouts?.count ?? 0,
-    };
+  return {
+    payoutsError: error,
+    payouts: results,
+    isPayoutsPending: isLoading,
+    setPayoutSearchTerm: setSearchTerm,
+    setPayoutPageNumber: setPageNumber,
+    payoutPageNumber: pageNumber,
+    payoutSearchTerm: searchTerm,
+    totalPayoutCount: potPayouts?.count ?? 0,
+  };
 };
