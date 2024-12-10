1d86c5069b254838bd4a32d4a66d8fc527d34ef8
Jiku Godwill Nsanwi <jikugodwill@yahoo.com>
2024-12-10 07:57:27 +0100

    Implemented search

--- src/pages/pot/[potId]/applications.tsx
+++ src/pages/pot/[potId]/applications.tsx
@@ -1,14 +1,19 @@
-import { ReactElement, useCallback, useEffect, useState } from "react";
+import { ReactElement, useCallback, useEffect, useMemo, useState } from "react";
 
+import { DialogOverlay } from "@radix-ui/react-dialog";
+import { Dot } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { MdCommentsDisabled } from "react-icons/md";
+import { sum } from "remeda";
 import { styled } from "styled-components";
 
+import { PotApplication, indexer } from "@/common/api/indexer";
 import { usePot } from "@/common/api/indexer/hooks";
 import { SearchIcon } from "@/common/assets/svgs";
 import CheckIcon from "@/common/assets/svgs/CheckIcon";
 import { Application, potClient } from "@/common/contracts/core";
-import { daysAgo, truncate } from "@/common/lib";
+import { daysAgo, toChronologicalOrder, truncate } from "@/common/lib";
 import {
   Button,
   FilterChip,
@@ -24,6 +29,8 @@ import { AccountOption, AccountProfilePicture } from "@/entities/account";
 import routesPath from "@/entities/core/routes";
 import { PotFilters } from "@/entities/pot";
 import { useProfileData } from "@/entities/profile";
+import { ProjectListingStatusVariant } from "@/entities/project";
+import { ProjectCardSkeleton } from "@/entities/project/components/ProjectCardSkeleton";
 import { PotApplicationReviewModal, potApplicationFiltersTags } from "@/features/pot-application";
 import { PotLayout } from "@/layout/PotLayout";
 import { useGlobalStoreSelector } from "@/store";
@@ -33,7 +40,6 @@ const Container = styled.div`
   display: flex;
   margin-bottom: 4rem;
   flex-direction: column;
-  gap: 2rem;
   .dropdown {
     display: none;
   }
@@ -44,71 +50,12 @@ const Container = styled.div`
     }
   }
 `;
-
-const Filter = styled.div`
-  display: grid;
-  width: 286px;
-  border-radius: 6px;
-  padding: 8px 0;
-  border: 1px solid var(--Neutral-500, #7b7b7b);
-  height: fit-content;
-  .item {
-    display: flex;
-    align-items: center;
-    gap: 12px;
-    padding: 0.5rem 1rem;
-    font-size: 14px;
-    cursor: pointer;
-    svg {
-      opacity: 0;
-      transition: all 300ms ease;
-    }
-    &.active {
-      svg {
-        opacity: 1;
-      }
-    }
-    &:hover {
-      svg {
-        opacity: 1;
-      }
-    }
-  }
-  .count {
-    color: #7b7b7b;
-    margin-left: auto;
-  }
-  @media only screen and (max-width: 768px) {
-    display: none;
-  }
-`;
-
 const ApplicationsWrapper = styled.div`
   border-radius: 6px;
   display: flex;
   flex-direction: column;
   width: 100%;
 `;
-
-// const SearchBar = styled.div`
-//   display: flex;
-//   svg {
-//   }
-//   input {
-//     font-size: 14px;
-//     background: none;
-//     width: 100%;
-//     height: 100%;
-//     padding: 10px 10px 10px 10px;
-//     border: none;
-//     outline: none;
-//   }
-//   @media only screen and (max-width: 768px) {
-//     input {
-//       padding: 8px 24px 8px 54px;
-//     }
-//   }
-// `;
 
 const ApplicationCard = styled.div`
   .header {
@@ -143,55 +90,43 @@ const ApplicationCard = styled.div`
       color: #292929;
     }
   }
-  .content {
+  .main {
     display: flex;
     flex-direction: column;
     gap: 1rem;
-    margin-left: 70px;
-    font-size: 17px;
-    .message {
-      word-wrap: break-word;
-      overflow-wrap: break-word;
-      word-break: break-word;
-      max-width: 100%;
-      a {
-        word-break: break-word;
-        display: inline-block;
-        max-width: 100%;
-      }
-    }
-    .notes {
+    flex: 1;
+    .content {
       display: flex;
       flex-direction: column;
-      gap: 8px;
-      .title {
-        color: #7b7b7b;
+      font-size: 17px;
+      .message {
+        word-wrap: break-word;
+        overflow-wrap: break-word;
+        word-break: break-word;
+        max-width: 100%;
+        a {
+          word-break: break-word;
+          display: inline-block;
+          max-width: 100%;
+        }
       }
-    }
-    button {
-      width: fit-content;
+      .notes {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+        .title {
+          color: #7b7b7b;
+        }
+      }
+      button {
+        width: fit-content;
+      }
     }
   }
 `;
 
 const Status = styled.div`
   display: flex;
-`;
-
-const DropdownLabel = styled.div`
-  display: flex;
-  gap: 10px;
-  align-items: center;
-  .label {
-    font-weight: 500;
-  }
-  .count {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 50%;
-    background: #ebebeb;
-  }
 `;
 
 const ApplicationsTab = () => {
@@ -210,16 +145,36 @@ const ApplicationsTab = () => {
   const owner = potDetail?.owner?.id || "";
   const admins = potDetail?.admins.map((adm) => adm.id) || [];
   const chef = potDetail?.chef?.id || "";
+  const [statusFilter, setStatusFilter] = useState<ProjectListingStatusVariant>("All");
+  const [pageNumber, setPageNumber] = useState(1);
+  const [searchTerm, setSearchTerm] = useState<string | undefined>(undefined);
+
+  const {
+    isLoading: areApplicationsLoading,
+    error,
+    data: apps,
+  } = indexer.usePotApplications({
+    potId,
+    status: statusFilter === "All" ? undefined : statusFilter,
+    search: searchTerm,
+  });
+
+  const results = useMemo(() => {
+    const oldToRecent = toChronologicalOrder("submitted_at", apps?.results ?? []);
+    return oldToRecent.toReversed();
+  }, [apps?.results]);
 
   //! TODO: please use `indexer.usePotApplications` instead!
-  useEffect(() => {
-    // Fetch applications
-    (async () => {
-      const applicationsData = await potClient.getApplications({ potId });
-      setApplications(applicationsData);
-      setFilteredApplications(applicationsData);
-    })();
-  }, [potId]);
+  // useEffect(() => {
+  //   // if (!apps) return;
+  //   // Fetch applications
+  //   (async () => {
+  //     const applicationsData = await potClient.getApplications({ potId });
+  //     setApplications(applications);
+  //     setFilteredApplications(applicationsData);
+  //   })();
+  // }, [potId]);
+  console.log({ filteredApplications, results });
 
   // Handle update application status for web wallet
   // INFO: Not needed. There's a global transaction successful modal. But leaving it here just in case
@@ -251,7 +206,7 @@ const ApplicationsTab = () => {
 
   const handleApproveApplication = (projectId: string) => {
     setProjectId(projectId);
-    setProjectStatus("Approved");
+    setStatusFilter("Approved");
   };
 
   const handleRejectApplication = (projectId: string) => {
@@ -264,77 +219,77 @@ const ApplicationsTab = () => {
     setProjectStatus("");
   };
 
-  const searchApplications = (searchTerm: string) => {
-    // filter applications that match the search term (message, project_id, review_notes or status)
-    const filteredApplications = applications?.filter((application) => {
-      const { message, project_id, review_notes, status } = application;
-      const searchFields = [message, project_id, review_notes, status];
-      return searchFields.some((field) =>
-        field ? field.toLowerCase().includes(searchTerm.toLowerCase().trim()) : "",
-      );
-    });
-    return filteredApplications;
+  // const searchApplications = (searchTerm: string) => {
+  //   // filter applications that match the search term (message, id, review_notes or status)
+  //   const filteredApplications = applications?.filter((application) => {
+  //     const { message, id, review_notes, status } = application;
+  //     const searchFields = [message, id, review_notes, status];
+  //     return searchFields.some((field) =>
+  //       field ? field.toLowerCase().includes(searchTerm.toLowerCase().trim()) : "",
+  //     );
+  //   });
+  //   return filteredApplications;
+  // };
+
+  const getApplicationCount = (status: string) => {
+    return apps?.results.filter((app) => app.status === status).length;
   };
 
-  const getApplicationCount = useCallback(
-    (sortVal: string) => {
-      if (!applications) return;
-      return applications?.filter((application: any) => {
-        if (sortVal === "All") return true;
-        return application.status === sortVal;
-      })?.length;
-    },
-    [applications],
-  );
-
   const applicationsFilters: Record<string, { label: string; val: string; count?: number }> = {
-    ALL: {
+    All: {
       label: "All",
-      val: "ALL",
-      count: getApplicationCount("All")!,
+      val: "all",
+      count: apps?.count,
     },
-    APPROVED: {
+    Approved: {
       label: "Approved",
-      val: "APPROVED",
+      val: "approved",
       count: getApplicationCount("Approved")!,
     },
-    PENDING: {
+    Pending: {
       label: "Pending",
-      val: "PENDING",
+      val: "pending",
 
       count: getApplicationCount("Pending")!,
     },
-    REJECTED: {
+    Rejected: {
       label: "Rejected",
-      val: "REJECTED",
+      val: "rejected",
       count: getApplicationCount("Rejected")!,
     },
   };
 
-  const [searchTerm, setSearchTerm] = useState("");
+  // const [searchTerm, setSearchTerm] = useState("");
 
-  const sortApplications = (key: string) => {
-    if (key === "ALL") {
-      return searchApplications(searchTerm);
-    }
-    const filtered = applications?.filter((application: any) => {
-      return application.status === applicationsFilters[key].label.split(" ")[0];
-    });
-    return filtered;
-  };
+  // const sortApplications = (key: string) => {
+  //   if (key === "ALL") {
+  //     return searchApplications(searchTerm);
+  //   }
+  //   const filtered = applications?.filter((application: any) => {
+  //     return application.status === applicationsFilters[key].label.split(" ")[0];
+  //   });
+  //   return filtered;
+  // };
 
   const isChefOrGreater =
     accountId === chef || admins.includes(accountId || "") || accountId === owner;
 
-  const [filterValue, setFilterValue] = useState("ALL");
-  const handleSort = (key: string) => {
-    const sorted = sortApplications(key);
-    setFilteredApplications(sorted);
-    setFilterValue(key);
-  };
+  // const [filterValue, setFilterValue] = useState("ALL");
+  // const handleSort = (key: string) => {
+  //   const sorted = sortApplications(key);
+  //   setFilteredApplications(sorted);
+  //   setFilterValue(key);
+  // };
+
+  useEffect(() => {
+    if (error) {
+      console.log(error);
+    }
+    console.log({ statusFilter });
+  }, [statusFilter, error]);
 
   return (
-    <Container>
+    <Container className="gap-6">
       {/* Modal */}
       <PotApplicationReviewModal
         open={!!projectId}
@@ -346,8 +301,10 @@ const ApplicationsTab = () => {
       <div className="flex gap-3">
         {Object.keys(applicationsFilters).map((key) => (
           <FilterChip
-            variant={filterValue === key ? "brand-filled" : "brand-outline"}
-            onClick={() => handleSort(key)}
+            variant={statusFilter === key ? "brand-filled" : "brand-outline"}
+            onClick={() =>
+              setStatusFilter(applicationsFilters[key].label as ProjectListingStatusVariant)
+            }
             className="font-medium"
             label={applicationsFilters[key].label}
             count={applicationsFilters[key].count}
@@ -355,24 +312,29 @@ const ApplicationsTab = () => {
           />
         ))}
       </div>
-      <ApplicationsWrapper>
-        <SearchBar />
-        <div className="grid w-full grid-cols-1 gap-5 md:grid-cols-2">
-          {filteredApplications.length ? (
-            filteredApplications.map((application) => {
-              return (
-                <ApplicationData
-                  key={application.project_id}
-                  applicationData={application}
-                  isChefOrGreater={isChefOrGreater}
-                  handleApproveApplication={handleApproveApplication}
-                  handleRejectApplication={handleRejectApplication}
-                />
-              );
-            })
-          ) : (
-            <div style={{ padding: "1rem" }}>No applications to display</div>
-          )}
+      <ApplicationsWrapper className="gap-6">
+        <SearchBar
+          placeholder="Search Applications"
+          onChange={({ target }) => setSearchTerm(target.value.toLowerCase())}
+          defaultValue={searchTerm}
+        />
+        <div className="flex w-full flex-col flex-wrap justify-between gap-5 md:flex-row">
+          {
+            // filteredApplications.length ? (
+            results.map((application: PotApplication) => (
+              <ApplicationData
+                key={application.id}
+                applicationData={application}
+                isChefOrGreater={isChefOrGreater}
+                handleApproveApplication={handleApproveApplication}
+                handleRejectApplication={handleRejectApplication}
+                isLoading={areApplicationsLoading}
+              />
+            ))
+            // ) : (
+            //   <div style={{ padding: "1rem" }}>No applications to display</div>
+            // )
+          }
         </div>
       </ApplicationsWrapper>
     </Container>
@@ -384,29 +346,37 @@ const ApplicationData = ({
   isChefOrGreater,
   handleApproveApplication,
   handleRejectApplication,
+  isLoading: areApplicationsLoading,
 }: {
-  applicationData: Application;
+  applicationData: PotApplication;
   isChefOrGreater: boolean;
   handleApproveApplication: (projectId: string) => void;
   handleRejectApplication: (projectId: string) => void;
+  isLoading: boolean;
 }) => {
-  const { project_id, status, message, review_notes, submitted_at } = applicationData;
-  const { borderColor, color, icon, label, background } = potApplicationFiltersTags[status];
-  const { profile } = useProfileData(project_id, true, false);
+  const { applicant, status, message, submitted_at } = applicationData;
+  const submittedTimeStamp = new Date(submitted_at).getTime();
+  const { id: projectId } = applicant;
+  const review_notes = null;
 
-  return (
+  const { borderColor, color, icon, label, background } = potApplicationFiltersTags[status];
+  const { profile } = useProfileData(projectId, true, false);
+
+  return areApplicationsLoading ? (
+    <ProjectCardSkeleton />
+  ) : (
     <ApplicationCard
-      key={project_id}
-      className="flex min-w-[234px] flex-col items-start justify-start gap-4 rounded-2xl border border-[#eaeaea] bg-white p-5 md:min-w-[600px]"
+      key={projectId}
+      className="mx-auto flex min-w-[234px] max-w-[715px] flex-1 flex-col items-start justify-start gap-4 rounded-2xl border border-[#eaeaea] bg-white p-5 md:min-w-[425px]"
     >
       <div className="header">
         <div className="header-info">
           <AccountOption
             title="user Account"
-            accountId={project_id}
+            accountId={projectId}
             highlightOnHover={true}
             isRounded={true}
-            daysAgoData={submitted_at}
+            daysAgoData={submittedTimeStamp}
           />
         </div>
         <Status
@@ -431,27 +401,71 @@ const ApplicationData = ({
           </div>
         </Status>
       </div>
-      <div className="content text-neutral-600">
-        <div className="message">{message}</div>
-        {review_notes && (
-          <div className="notes">
-            <div className="title">Admin notes:</div>
-            <div>{review_notes}</div>
-          </div>
-        )}
-        {isChefOrGreater && (
-          <>
-            {status !== "Approved" && (
-              <Button variant="tonal-filled" onClick={() => handleApproveApplication(project_id)}>
-                Approve
-              </Button>
-            )}
-            {status !== "Rejected" && (
-              <Button onClick={() => handleRejectApplication(project_id)}>Reject</Button>
-            )}
-          </>
-        )}
+      <div className="main ml-0 md:ml-12">
+        <div className="content text-neutral-600">
+          <div className="message">{message}</div>
+        </div>
       </div>
+      {status !== "Pending" && (
+        <div className="ml-0 self-stretch md:ml-12">
+          <div
+            className={cn(
+              "inline-flex w-full flex-col items-start justify-start gap-3 rounded-xl p-5",
+              { "bg-[#fef3f2]": status === "Rejected", "bg-[#f7f7f7]": status !== "Rejected" },
+            )}
+          >
+            <div className="title flex items-center">
+              <div className="flex items-center gap-1">
+                <AccountProfilePicture
+                  accountId={projectId}
+                  className="h-6 w-6 rounded-full shadow-inner"
+                />
+                {profile?.name ? (
+                  <div className="name text-[17px] font-semibold leading-normal text-[#292929]">
+                    {truncate(profile?.name, 15)}
+                  </div>
+                ) : (
+                  <div>{truncate(projectId, 15)}</div>
+                )}
+              </div>{" "}
+              <Dot className="text-[#010101]/50" />
+              <div className="text-[17px] font-semibold text-[#010101]/50 ">Admin</div>
+            </div>
+            {review_notes ? (
+              <div className="text-[17px] font-normal leading-tight text-[#010101]/70">
+                {review_notes}
+              </div>
+            ) : (
+              <div
+                className={
+                  "mx-auto inline-flex w-full items-center justify-center gap-3 rounded-lg bg-white p-2 text-[17px] font-medium text-[#7a7a7a]"
+                }
+              >
+                {" "}
+                <MdCommentsDisabled className="h-6 w-6 text-[#7B7B7B]" />
+                <p>No coment</p>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+      {isChefOrGreater && (
+        <div className="flex gap-4 self-end">
+          {status !== "Approved" && (
+            <Button
+              variant={"standard-outline"}
+              onClick={() => handleApproveApplication(projectId)}
+            >
+              Accept
+            </Button>
+          )}
+          {status !== "Rejected" && (
+            <Button variant={"standard-outline"} onClick={() => handleRejectApplication(projectId)}>
+              Reject
+            </Button>
+          )}
+        </div>
+      )}
     </ApplicationCard>
   );
 };
