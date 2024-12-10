6a4d0317ae99bda01361eef7c926225643cd31ad
Jiku Godwill Nsanwi <jikugodwill@yahoo.com>
2024-12-10 10:02:36 +0100

    updated import

--- src/features/pot-application/index.ts
+++ src/features/pot-application/index.ts
@@ -2,4 +2,4 @@ export * from "./components/PotApplicationModal";
 export * from "./components/PotApplicationReviewModal";
 export * from "./components/tags";
 export * from "./hooks/clearance";
-export * from "./components/potApplicationCardSkeleton";
+export * from "./components/PotApplicationCardSkeleton";
