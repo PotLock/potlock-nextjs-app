4a97e9962f07ad9d1e899252f874742a169021fd
Ebube <ebubeagwaze@gmail.com>
2025-01-14 18:32:06 +0100

    made approved a constant status for create

--- src/common/contracts/core/lists/client.ts
+++ src/common/contracts/core/lists/client.ts
@@ -39,13 +39,11 @@ export const create_list = ({
   image_cover_url,
   accounts,
   allowApplications,
-  approveApplications,
 }: {
   name: string;
   description: string;
   admins: AccountId[];
   allowApplications?: boolean;
-  approveApplications?: boolean;
   accounts: { registrant_id: AccountId; status: RegistrationStatus }[];
   image_cover_url?: string | null;
 }) => {
@@ -58,9 +56,7 @@ export const create_list = ({
       cover_image_url: image_cover_url ?? null,
       ...(accounts?.length && { registrations: accounts }),
       admin_only_registrations: !allowApplications,
-      default_registration_status: approveApplications
-        ? RegistrationStatus.Approved
-        : RegistrationStatus.Pending,
+      default_registration_status: RegistrationStatus.Approved,
     },
     deposit: floatToYoctoNear(0.02 + accountsDeposit),
     gas: "300000000000000",
