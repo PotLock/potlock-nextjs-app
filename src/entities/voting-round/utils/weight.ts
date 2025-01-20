69a4ca0cd6f17a5380846b153ee741bacbec71eb
Carina.Akaia.io <cvo.akaia@gmail.com>
2025-01-20 23:41:50 +0000

    Remove hardcoded hotfix

--- src/entities/voting-round/utils/weight.ts
+++ src/entities/voting-round/utils/weight.ts
@@ -76,10 +76,6 @@ export const getVoteWeight = (
       default: {
         if (profileParameter instanceof Big) {
           isApplicable = profileParameter[rule.comparator](rule.threshold);
-
-          // TODO: Remove after backend fix
-          //! TEMPORARILY mitigate the issue where the backend kept track of the staked amount even after the voting round
-          isApplicable = voterProfile.accountId === "satoukibijusu.near" ? true : isApplicable;
         }
       }
     }
