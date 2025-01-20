b11b488f14b43faacc887a04209d6b8c7ac61533
Jiku Godwill Nsanwi <jikugodwill@yahoo.com>
2025-01-20 03:55:51 +0100

    updated for addFundingSources schema object

--- src/features/profile-setup/models/schemas.ts
+++ src/features/profile-setup/models/schemas.ts
@@ -1,5 +1,29 @@
 import { array, boolean, object, string } from "zod";
 
+export const addFundingSourceSchema = object({
+  investorName: string({
+    required_error: "Please enter the investor name.",
+  })
+    .min(3, "Must be at least 3 characters")
+    .max(50, "Must be less than 50 characters long"),
+
+  date: string().optional(),
+
+  description: string({
+    required_error: "Please enter description.",
+  }).max(500, "Must be less than 500 characters long"),
+
+  amountReceived: string({
+    required_error: "Please enter the investment amount.",
+  }),
+
+  denomination: string({
+    required_error: "Please enter the denomination.",
+  })
+    .min(3, "Must be at least 3 characters")
+    .max(50, "Must be less than 50 characters"),
+});
+
 export const projectEditorSchema = object({
   name: string()
     .min(3, "Must be at least 3 characters long")
@@ -23,43 +47,11 @@ export const projectEditorSchema = object({
 
   smartContracts: array(array(string())).optional(),
 
-  fundingSources: array(
-    object({
-      investorName: string(),
-      date: string().optional(),
-      description: string(),
-      amountReceived: string(),
-      denomination: string(),
-    }),
-  ).optional(),
+  fundingSources: array(addFundingSourceSchema).optional(),
 
   githubRepositories: array(string()).optional(),
   website: string().optional(),
   twitter: string().optional(),
   telegram: string().optional(),
   github: string().optional(),
-});
-
-export const addFundingSourceSchema = object({
-  investorName: string({
-    required_error: "Please enter the investor name.",
-  })
-    .min(3, "Must be at least 3 characters")
-    .max(50, "Must be less than 50 characters long"),
-
-  date: string().optional(),
-
-  description: string({
-    required_error: "Please enter description.",
-  }).max(500, "Must be less than 500 characters long"),
-
-  amountReceived: string({
-    required_error: "Please enter the investment amount.",
-  }),
-
-  denomination: string({
-    required_error: "Please enter the denomination.",
-  })
-    .min(3, "Must be at least 3 characters")
-    .max(50, "Must be less than 50 characters"),
 });
