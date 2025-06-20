87d6a177ef0e1524133d1f45b5e2172464eb1265
Carina Akaia <cvo.akaia@gmail.com>
2025-06-20 11:58:57 +0000

    wip

---
+++ src/entities/_shared/account/hooks/social-image.ts
@@ -0,0 +1,38 @@
+import { useMemo } from "react";
+
+import { NOOP_STRING } from "@/common/constants";
+import type { NEARSocialUserProfile } from "@/common/contracts/social-db";
+import { nftContractHooks } from "@/common/contracts/tokens";
+
+export type UseAccountSocialImageParams = {
+  data?: NEARSocialUserProfile["image"];
+  fallbackUrl: string;
+};
+
+export const useAccountSocialImageUrl = ({ data, fallbackUrl }: UseAccountSocialImageParams) => {
+  const nftParams = useMemo(
+    () =>
+      data !== undefined && typeof data !== "string" && "nft" in data ? (data.nft ?? null) : null,
+
+    [data],
+  );
+
+  const { data: nft } = nftContractHooks.useToken({
+    enabled: nftParams !== null,
+    contractAccountId: nftParams?.contractId ?? NOOP_STRING,
+    tokenId: nftParams?.tokenId ?? NOOP_STRING,
+  });
+
+  const rawImageSrc = useMemo(() => {
+    if (typeof data !== "string") {
+      return (
+        data?.url ?? (data?.ipfs_cid ? `https://ipfs.near.social/ipfs/${data?.ipfs_cid}` : null)
+      );
+    } else return data;
+  }, [data]);
+
+  return useMemo(
+    () => nft?.metadata.media ?? rawImageSrc ?? fallbackUrl,
+    [fallbackUrl, nft?.metadata.media, rawImageSrc],
+  );
+};
--- src/entities/_shared/account/hooks/social-profile.ts
+++ src/entities/_shared/account/hooks/social-profile.ts
@@ -1,14 +1,11 @@
-import { useMemo } from "react";
-
-import { NOOP_STRING } from "@/common/constants";
 import { socialDbContractHooks } from "@/common/contracts/social-db";
-import { nftContractHooks } from "@/common/contracts/tokens";
 import type { ByAccountId, ConditionalActivation } from "@/common/types";
 
 import {
   ACCOUNT_PROFILE_COVER_IMAGE_PLACEHOLDER_SRC,
   ACCOUNT_PROFILE_IMAGE_PLACEHOLDER_SRC,
 } from "../constants";
+import { useAccountSocialImageUrl } from "./social-image";
 
 export const useAccountSocialProfile = ({
   accountId,
@@ -22,65 +19,22 @@ export const useAccountSocialProfile = ({
     error,
   } = socialDbContractHooks.useSocialProfile({ enabled, accountId });
 
-  const avatarNftParams = useMemo(
-    () =>
-      data?.image !== undefined && typeof data.image !== "string" && "nft" in data.image
-        ? (data.image.nft ?? null)
-        : null,
-
-    [data?.image],
-  );
-
-  const { data: avatarNft } = nftContractHooks.useToken({
-    enabled: avatarNftParams !== null,
-    contractAccountId: avatarNftParams?.contractId ?? NOOP_STRING,
-    tokenId: avatarNftParams?.tokenId ?? NOOP_STRING,
+  const avatarSrc = useAccountSocialImageUrl({
+    data: data?.image,
+    fallbackUrl: ACCOUNT_PROFILE_IMAGE_PLACEHOLDER_SRC,
   });
 
-  console.log("avatarNft", avatarNft);
-
-  const avatarSrc = useMemo(() => {
-    if (avatarNft !== undefined) {
-      return avatarNft.metadata.media;
-    } else if (typeof data?.image === "string") {
-      return data?.image;
-    } else {
-      return (
-        data?.image?.url ??
-        (data?.image?.ipfs_cid ? `https://ipfs.near.social/ipfs/${data?.image?.ipfs_cid}` : null)
-      );
-    }
-  }, [avatarNft, data?.image]);
-
-  const backgroundNft = useMemo(
-    () =>
-      data?.backgroundImage !== undefined &&
-      typeof data.backgroundImage !== "string" &&
-      "nft" in data.backgroundImage
-        ? (data.backgroundImage.nft ?? null)
-        : null,
-
-    [data?.backgroundImage],
-  );
-
-  const backgroundSrc = useMemo(
-    () =>
-      typeof data?.backgroundImage === "string"
-        ? data?.backgroundImage
-        : (data?.backgroundImage?.url ??
-          (data?.backgroundImage?.ipfs_cid
-            ? `https://ipfs.near.social/ipfs/${data?.backgroundImage?.ipfs_cid}`
-            : null)),
-
-    [data?.backgroundImage],
-  );
+  const backgroundSrc = useAccountSocialImageUrl({
+    data: data?.backgroundImage,
+    fallbackUrl: ACCOUNT_PROFILE_COVER_IMAGE_PLACEHOLDER_SRC,
+  });
 
   return {
     isLoading,
     isValidating,
     profile: data ?? undefined,
     avatarSrc: avatarSrc ?? ACCOUNT_PROFILE_IMAGE_PLACEHOLDER_SRC,
-    backgroundSrc: backgroundSrc ?? ACCOUNT_PROFILE_COVER_IMAGE_PLACEHOLDER_SRC,
+    backgroundSrc: backgroundSrc,
     refetch,
     error,
   };
